56a77f022fca8681d59e25456cab4ce1e894f5c3
Dmitry Eroshenko <airs0urce0@gmail.com>
2020-11-13 05:45:49 +0700

    handle edge case and small other changes

--- bicycleTurns.ino
+++ bicycleTurns.ino
@@ -38,16 +38,36 @@ void setup() {
 }
 
 unsigned long lastBtnPressOnLoopStart = 0;
+unsigned long loopActivityLast = 0;
 
 void loop() {
-  Serial.println("CHECK BUTTONS PRESSED");
+  //Serial.println("CHECK BUTTONS PRESSED");
+
+  if (millis() - loopActivityLast > 2000) {
+    /* when loop is running more than 2 seconds 
+     *  and user doesn't press buttons let's send arduino to sleep.
+     *  Why do we need this? It's possible that after calling goSleep() fucntion 
+     *  we will get arduino running instead os sleeping. It can happen in 
+     *  case when you pressed button down and then released it after more 
+     *  than 250ms (we have protection for 250ms in side goSleep).
+     *  So, when you release the button, contact bouncing triggers 
+     *  interruption and arduino wakes up because thinks you pressed button.
+     *  Later in loop we check if button pressed but no buttons pressed because 
+     *  you actually just released the button and because of this arduino can 
+     *  continue running forever and waste energy. For this case we have this handling 
+     *  to force arduino sleeping even if contact bouncing broke our logic.
+    */
+    goSleep();
+  }
   
   lastBtnPressOnLoopStart = lastBtnPress;
   if (isBtnPressed(BTN_LEFT)) {
+    loopActivityLast = millis();
     if (state == 1) { // now blinking after pressed left
       // stop left blinking and go sleep
       state = 0;
       goSleep();
+      loopActivityLast = millis();
     } else if (state == 2) { // now blinking after pressed right
       // stop blinking right and start left
       state = 1;
@@ -56,12 +76,13 @@ void loop() {
       state = 1;
       blinkTurn(LED_LEFT);
     }
-    
   } else if (isBtnPressed(BTN_RIGHT)) {
+    loopActivityLast = millis();
     if (state == 2) { // now blinking after pressed right
       // stop right blinking and go sleep
       state = 0;
       goSleep();
+      loopActivityLast = millis();
     } else if (state == 1) { // now blinking after pressed left
       // stop blinking left and start right
       state = 2;
@@ -71,11 +92,13 @@ void loop() {
       blinkTurn(LED_RIGHT);
     }
   }
+
+  
 }
 
 
 void goSleep() {
-  Serial.println("GOING TO SLEEP");Serial.flush();
+  //Serial.println("GOING TO SLEEP");Serial.flush();
 
   // remove interruptions to make sure buttons bouces will not wake up
   // the microcontroller on moment when you remove finger from pressed button
@@ -88,12 +111,12 @@ void goSleep() {
   attachInterrupt(digitalPinToInterrupt(BTN_RIGHT), isrHandler, FALLING);
   LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
     
-  Serial.println("AWAKE");Serial.flush();
+  //Serial.println("AWAKE");Serial.flush();
 }
 
 bool isBtnPressed(byte btnPin) {
   if (digitalRead(btnPin) == LOW) {
-    delay(5);
+    delay(10);
     if (digitalRead(btnPin) == LOW) {
       return true;
     }
@@ -108,15 +131,15 @@ void isrHandler() {
   }
 }
 
-void blinkTurn(byte pin) {  
+void blinkTurn(byte ledPin) { 
   bool finished;
   while (true) {
-    finished = ledOn(pin);
+    finished = ledOn(ledPin);
     if (!finished) {
       break;
     } else {
       delay(120);
-      finished = ledOff(pin);
+      finished = ledOff(ledPin);
       if (!finished) {
         break;
       }
@@ -126,30 +149,31 @@ void blinkTurn(byte pin) {
 }
 
 // returns: true - finished, false - interrupted
-bool ledOn(byte pwmPin) {
-  for (int i = 0; i <= 255; i += 2) {
+bool ledOn(byte led) {
+  for (int i = 0; i <= 255; i += 1) {
     if (btnPressedAgain()) {
-      analogWrite(pwmPin, 0);
+      analogWrite(led, 0);
       return false;
     }
-    analogWrite(pwmPin, (i > 255 ? 255: i));
+    analogWrite(led, i);
     delay(1);
   }
-  analogWrite(pwmPin, 255);
+  
+  analogWrite(led, 255);
   return true;
 }
 
 // returns: true - finished, false - interrupted
-bool ledOff(byte pwmPin) {
-  for (int i = 255; i >= 0; i -= 2) {
+bool ledOff(byte led) {
+  for (int i = 255; i >= 0; i -= 1) {
     if (btnPressedAgain()) {
-      analogWrite(pwmPin, 0);
+      analogWrite(led, 0);
       return false;
     }
-    analogWrite(pwmPin, (i < 0 ? 0: i)); 
+    analogWrite(led, i);
     delay(1);
   }
-  analogWrite(pwmPin, 0);
+  analogWrite(led, 0);
   return true;
 }
 
